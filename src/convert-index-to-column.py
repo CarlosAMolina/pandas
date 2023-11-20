336a49f217658d65c9600e01c0e01d9e9cc20186
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-11-20 21:01:05 +0100

    Drop unrequired code

--- src/convert-index-to-column.py
+++ src/convert-index-to-column.py
@@ -24,7 +24,6 @@ expected_result_df = pd.DataFrame(
     index=index,
 )
 expected_result_df.columns = expected_result_df.columns.set_names("month")
-expected_result_df.to_csv("foo.csv")
 
 
 class ManualDfConverter:
@@ -38,38 +37,6 @@ class ManualDfConverter:
         column_names = ["january", "february", "march"]
         result = result.reindex(columns=column_names)
         return result
-        index = self._get_index()
-        data = self._get_data()
-        result = pd.DataFrame(
-            data=data,
-            index=index,
-        )
-        return result
-
-    def _get_index(self) -> list:
-        index = []
-        for indexes in self._df.index:
-            index_parent = indexes[0]
-            if index_parent not in index:
-                index += [index_parent]
-        return index
-
-    def _get_data(self) -> dict:
-        # https://stackoverflow.com/questions/44823418/multi-index-pandas-dataframe-to-a-dictionary/44823657#44823657
-        index_parent_name = self._df.index.names[0]
-        df_without_parent_index = self._df.reset_index(
-            level=[index_parent_name], drop=True
-        )
-        df_without_original_index = df_without_parent_index.reset_index()
-        data_array = df_without_original_index.to_dict(orient="split")["data"]
-        data = dict()
-        for column_and_value in data_array:
-            column, value = column_and_value
-            if column in data.keys():
-                data[column] += [value]
-            else:
-                data[column] = [value]
-        return data
 
 
 if __name__ == "__main__":

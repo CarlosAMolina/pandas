8d08e39b393d855d1abbb48d9d5ffce9cb7d1452
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-08-21 07:06:44 +0200

    apply pre-commit

--- src/filter_boolean.py
+++ src/filter_boolean.py
@@ -2,26 +2,28 @@ from pandas import DataFrame as Df
 from pandas import Series
 
 users_df = Df(
-   [
-       [1, True],
-       [2, False],
-       [3, True],
-   ],
-   columns=["id", "is_adult"],
+    [
+        [1, True],
+        [2, False],
+        [3, True],
+    ],
+    columns=["id", "is_adult"],
 )
+
 
 def print_is_expected_condition(condition: Series):
     if isinstance(condition, Series) and condition.to_list() == [True, False, True]:
-        print('ok')
+        print("ok")
     else:
-        print('wrong')
+        print("wrong")
+
 
 # Option 1
 condition_equals = users_df.is_adult == True
 print_is_expected_condition(condition_equals)
 # Option 2
 # The following condition has bool type, not Series.
-condition_is = users_df.is_adult is True 
+condition_is = users_df.is_adult is True
 print_is_expected_condition(condition_is)
 # Option 3
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.eq.html
